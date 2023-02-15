7642ff794e2a4266c7da26068bfaae69a14f7610
GiselleEmilia <114619090+GiselleEmilia@users.noreply.github.com>
2023-02-15 16:11:28 -0700

    API CHANGES T

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
+
+
 var express = require('express')
 var bodyParser = require('body-parser')
 
 var app = express()
-
 app.use(bodyParser.text({
 
     type:function(req)
@@ -11,30 +13,27 @@ app.use(bodyParser.text({
     }
 }));
 
-app.post('/',(req,res)=>
+
+
+app.post('/', (req,res)=>
 {
-
     console.log(req.body)
-    res= res.status(200)
+    res=res.status(200)
     var contentType = req.get('Content-Type');
-    if(contentType){
+    if(contentType)
+    {
+        console.log("Content-Type:"+ contentType);
 
-        console.log("Content type:" + contentType)
+        res=res.type(contentType)
     }
 
     res.send(req.body)
+
 })
 
 app.listen(process.env.PORT || 8080 );
 
 
+module.exports = app;// for testing
 
-
-module.exports = app; // for testing
-
-//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
-
-
-
-
-
+//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8080
